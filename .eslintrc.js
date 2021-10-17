68d17047ba7a95c85806951c164f08c161b4c3be
sagie gur ari <sagiegurari@gmail.com>
2021-10-17 15:03:42 +0000

    replace task runner

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,31 @@
-'use strict';
-
-const commons = require('js-project-commons');
-
-module.exports = commons.lint.eslint.web;
+module.exports = {
+    'env': {
+        'browser': true,
+        'node': true,
+        'commonjs': true,
+        'es2021': true,
+        'mocha': true
+    },
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'ecmaVersion': 13
+    },
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ]
+    }
+};
